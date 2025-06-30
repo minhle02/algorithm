fe1ae9b25488564f366fdb7d6d9890281fea3979
minhle02 <85542294+minhle02@users.noreply.github.com>
2025-06-30 07:29:48 +0700

    Add testcase_gen to past exercise

---
+++ Past-Exercise/Codejam/FinalRound1-2025/testcase_gen.py
@@ -0,0 +1,17 @@
+import random
+
+class InputGenerator:
+    def __init__(self):
+        pass 
+
+    def gen_input_prob_A(self) -> str:
+        N = random.randint(1, 10)
+        M = random.randint(1, 10)
+        data = f"{N} {M}\n"
+        for _ in range(N):
+            data += f"{random.randint(1,20)} "
+        data += "\n"
+        for _ in range(M):
+            data += f"{random.randint(1, 20)} "
+        data += "\n"
+        return data
